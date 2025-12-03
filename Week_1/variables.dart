95e08c80f09dd6a6b18529affa71e6d66e06f86b
prodigy-genes <bulletoze@gmail.com>
2025-12-03 16:05:08 +0000

    Daily Progress Log Report plus Week 2 Challenge in main.dart

---
+++ Week_1/variables.dart
@@ -0,0 +1,27 @@
+var  x = 'I am an infered variable string ';
+String y = 'I am an explicit variable string ';
+
+int a = 10;
+double b = 20.5;
+
+bool isTrue = true;
+
+var z = 10;
+
+dynamic d = 10; // can hold any type of value
+
+
+void main() {
+  print(x);
+  print(y);
+
+  print(a);
+  print(b);
+
+  print(isTrue);
+
+  print(z);
+
+  print(d);
+}
+  

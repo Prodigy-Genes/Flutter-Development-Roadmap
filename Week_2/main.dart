f7510324b420e2db80f4b16c8cff1570877ecc5f
prodigy-genes <bulletoze@gmail.com>
2025-12-03 15:56:23 +0000

    Daily Progress Log Report plus Week 2 Challenge in main.dart

--- Week_2/main.dart
+++ Week_2/main.dart
@@ -1,139 +1,67 @@
 // Week 2 Assignment : Create a simple library management system with classes for books, members, and loans, demonstrating OOP principles.
 
-// I decided to add some bit of authentication to the system using mixins for digital access.
-import 'dart:io';
+abstract class Library {
 
-abstract class LibraryItem{
   String title;
-  LibraryItem(this.title);
-  
+  Library(this.title);
+
+  void checkin();
   void checkout();
-  void returnItem();
 }
 
-class Book extends LibraryItem{
+class Books extends Library {
+
   String author;
-  Book(String title, this.author) : super(title);
+  Books(String title, this.author): super(title);
+
+  @override
+  void checkin(){
+    print("Checking in a book $title by $author. ");
+  }
   @override
   void checkout(){
-    print("Checking out book: $title by $author");
-    
-  }
-  @override
-  void returnItem(){
-    print("Returning book: $title by $author");
+    print("Checking out a book $title by $author. ");
   }
 }
 
-mixin DigitalAccess{
-  bool isLoggedIn = false;
-  String? Currentusername;
-
-  void login(){
-    stdout.write("Enter your username: ");
-    String username = stdin.readLineSync()!;
-    stdout.write("Enter your password: ");
-    String password = stdin.readLineSync()!;
-
-    Map<String, String> userCredentials = {
-      "prodigygenes": "password1",
-      "joseph": "password2",
-    };
-
-    if (userCredentials.containsKey(username) && userCredentials[username] == password) {
-      isLoggedIn =true;
-      Currentusername = username;
-      print("Login successful!");
-      print("Accessing digital content...");
-      print("Welcome to the digital library! $username Enjoy!");
-    } else {
-      print("Invalid username or password. Please try again.");
-    }
-  }
-}
-
-class Ebook extends LibraryItem with DigitalAccess{
-  String author;
-  Ebook(String title, this.author) : super(title);
-
-  @override
-  void checkout(){
-    if(isLoggedIn){
-      print("$Currentusername is Checking out ebook: $title by $author");
-    }else{
-      print("Please login first to access digital content.");
-    }
-  }
-
-  @override
-  void returnItem(){
-    if(isLoggedIn){
-      print("$Currentusername is Returning ebook: $title by $author");
-    }else{
-      print("Please login first to access digital content.");
-    }
-  }
-}
-
-class Member {
+class Members{
   String name;
-  Member(this.name);
 
-  List<LibraryItem> _Loans = [];
+  Members(this.name);
 
-  void borrowItem( LibraryItem item){
-    if(item is Ebook && !(item).isLoggedIn){
-      print("Please login to borrow digital items.");
-    }
-    item.checkout(); 
-    _Loans.add(item); 
-    print(" Member $name has borrowed ${item.title}.");
-    print("System: Added '${item.title}' from $name's account.\n");
+  List<Books> _borrowedbooks = [];
+
+  void Borrow(Books book){
+    book.checkout();
+    print("$name has checked out a book ${book.title} by ${book.author}");
+    _borrowedbooks.add(book);
+    print("Book has been added to borrowed books list");
   }
 
-  void returnItem(LibraryItem item){
-    if(_Loans.contains(item)){
-      item.returnItem();
-      _Loans.remove(item);
-      print("Member $name has returned ${item.title}.");
-      print("System: Removed '${item.title}' from $name's account.\n");
-    }else{
-      print("Member $name did not borrow ${item.title}.");
-    }
+  void Return(Books book){
+    book.checkin();
+    print("$name has returned book ${book.title} by ${book.author}");
+    _borrowedbooks.remove(book);
+    print("Book has been removed from borrowed books list");
   }
+
+  // to print the books in the list, since it is a private list, we need a getter
+  List<Books> get borrowedbooks => _borrowedbooks;
+
+  // Now a funtion to display it
+  void showList(){
+    print("Books borrowed includes: $borrowedbooks");
+  } 
 }
-
 
 void main(){
-  var myBook = Book("Anarchious", "Prodigygenes");
-  myBook.checkout();
-  myBook.returnItem();
 
-  
-  var myEbook = Ebook("My Vampire System", "JKS Manga");
-  
-  print("--- STEP 1: LOGIN ---");
-  // User logs into the Ebook system
-  myEbook.login(); 
+  var myBook = Books("Anarchious", "Prodigygenes");
+  var member1 = Members("Joseph");
 
-  // Create Member DYNAMICALLY based on who logged in
-  if (myEbook.isLoggedIn) {
-    // We take the username directly from the Ebook's mixin data
-    var activeMember = Member(myEbook.Currentusername!); 
-    
-    print("\n--- STEP 2: BORROWING ---");
-    // Now the member borrows items
-    activeMember.borrowItem(myEbook);
-     
+  member1.Borrow(myBook);
+  member1.Return(myBook);
 
-    print("\n--- STEP 3: RETURNING BOOk ---");
-    activeMember.returnItem(myEbook);
-  } else {
-    print("Session ended: No active member.");
-  }
-
-
-
-
- 
- }
+  // Show list of borrowed books
+  member1.showList();
+}
